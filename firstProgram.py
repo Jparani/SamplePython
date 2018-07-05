76c4e007956f993663509e3ca65c71ad3117db23
JParani <ramparani.1992@gmail.com>
2018-07-05 23:46:19 +0530

    first Commit - Python Program

---
+++ firstProgram.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import docx
+
+#print("Hello")
+#import os
+#os.path
+
+#doc=docx.Document('C:\Users\rampa\AppData\Local\Programs\Python\Python36-32.resume.docx')
+doc=docx.Document('resume.docx')
+doc.paragraphs
+
+
